21379fef4ef32e2979d539c662bdcc903587e47b
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-15 22:26:02 +0100

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+
+
+    // Write code below this lisne
+    function reusableFunction() {
+        console.log("Hi Padawan!");
+    }
+    // Write code above this line
+
+reusableFunction();
+module.exports = reusableFunction;
